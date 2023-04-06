c44ac52f440d572502b4c172bd71203bdd9a59bf
Christopher <ibebichi@hotmail.com>
2023-04-06 22:09:56 +0000

    Prime Number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,33 @@
 #include "main.h"
 
-/**
- * _sqrt_recursion - Returns the natural square root of a number
- *
- * @n: The number to find the square root of
- *
- * Return: The square root of n or -1 if n does not have a natural square root
- */
+int actual_sqrt_recursion(int n, int i);
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
-	if (n == 0 || n == 1)
-		return n;
-	int low = 1, high = n, mid, sqrt;
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
-		sqrt = n / mid;
-		if (mid == sqrt)
-			return mid;
-		else if (mid < sqrt)
-			low = mid + 1;
-		else
-			high = mid - 1;
-	}
-	return high;
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
